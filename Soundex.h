dbf55a7844ca600fef7245fe0b72ecb0ab03e136
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-09-11 15:51:37 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,31 +1,50 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
-#include <string>
-#include <cctype>
+#include "Soundex.h"
+#include <ctype.h>
+#include <string.h>
 
-char getSoundexCode(char c) 
-{
-    static const std::string codes = "01230120022455012623010202";
-    return std::isalpha(c) ? codes[std::toupper(c) - 'A'] : '0';
+// Helper function to get the Soundex code for a character
+char getSoundexCode(char c) {
+    c = toupper(c);
+    switch (c) {
+        case 'B': case 'F': case 'P': case 'V': return '1';
+        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
+        case 'D': case 'T': return '3';
+        case 'L': return '4';
+        case 'M': case 'N': return '5';
+        case 'R': return '6';
+        default: return '0'; // For A, E, I, O, U, H, W, Y
+    }
 }
 
-std::string generateSoundex(const std::string &name) {
-    std::string soundex(1, std::toupper(name[0]));
+// Function to generate Soundex code using map
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
     
-    for (std::size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex.back()) {
-            soundex += code;
+    // Set the first letter of the soundex as the uppercase version of the first character
+    soundex[0] = toupper(name[0]);
+
+    // Use map to apply getSoundexCode to all characters in name and store them in a temporary array
+    char tempSoundex[len];
+    tempSoundex[0] = soundex[0];
+    std::transform(name + 1, name + len, tempSoundex + 1, getSoundexCode);
+    
+    // Remove duplicates and '0' from tempSoundex while populating soundex
+    int sIndex = 1;
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        if (tempSoundex[i] != '0' && tempSoundex[i] != tempSoundex[i - 1]) {
+            soundex[sIndex++] = tempSoundex[i];
         }
     }
-
-    while (soundex.length() < 4) {
-        soundex += '0';
+    
+    // Pad with '0' if necessary
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
     }
 
-    return soundex;
+    soundex[4] = '\0';
 }
 
 #endif // SOUNDEX_H
-
