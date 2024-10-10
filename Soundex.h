693306bd819c040b3e763ea4258342a25ec0e0ac
PANKHURI-KAUSHIK253 <pankhurikaushik24@gmail.com>
2024-10-10 10:17:31 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,34 +1,33 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
-
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
 
-// Get the code from Table
-char getSoundexCodeFromTable(char c, const char *soundexTable)
+// Get the code from pattern
+char getSoundexCodeFromPattern(char c, const char *soundexPattern)
 {
-    c = toupper(c);
+    c = toUpper(c);
     if (c >= 'A' && c <= 'Z') {
-        return soundexTable[c - 'A'];
+        return soundexPattern[c - 'A'];
     }
     return '0'; // For any character outside A-Z
 }
 
 char getSoundexCode(char c) {
-    // Update the table with A to Z characters by numbers 
-    static const char soundexTable[26] = {
+    // Update the Pattern with A to Z characters by numbers 
+    static const char soundexPattern[26] = {
         '0', '1', '2', '3', '0', '1', '2', '0', // A-H
         '0', '2', '2', '4', '5', '5', '0', '1', // I-P
         '2', '6', '2', '3', '0', '1', '0', '2', // Q-X
         '0', '2'                                // Y-Z
     };
     
-    return getSoundexCodeFromTable(c, soundexTable);
+    return getSoundexCodeFromPattern(c, soundexPattern);
     
 }
 
-void validateCode(char code, char *soundex, int *sIndex)
+void verifyCode(char code, char *soundex, int *sIndex)
 {
     if (code != '0' && code != soundex[*sIndex - 1]) {
             soundex[(*sIndex)++] = code;
@@ -40,11 +39,11 @@ void processSoundex(const char *name, char *soundex, int *sIndex)
     int len = strlen(name);
     for (int i = 1; i < len && *sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        validateCode(code, soundex, sIndex);
+        processCode(code, soundex, sIndex);
     }
 }
 
-void padsWithZero(char *soundex, int sIndex)
+void padWithZero(char *soundex, int sIndex)
 {
     while (sIndex < 4) {
         soundex[sIndex++] = '0';
@@ -59,6 +58,11 @@ void generateSoundex(const char *name, char *soundex) {
 
     processSoundex(name, soundex, &sIndex);
     
+    padWithZero(soundex, sIndex);
+}
+
+#endif // SOUNDEX_H
+
     padsWithZero(soundex, sIndex);
 }
 
